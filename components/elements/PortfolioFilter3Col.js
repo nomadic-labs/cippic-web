a04773b42c0acfcc1a1accf88b6016bb93324b0e
Sharon <sharon.peishan.kennedy@gmail.com>
2023-11-07 10:59:02 -0500

    multiple filters

--- components/elements/PortfolioFilter3Col.js
+++ components/elements/PortfolioFilter3Col.js
@@ -1,34 +1,75 @@
 
-import Isotope from "isotope-layout"
 import Link from "next/link"
-import { useCallback, useEffect, useRef, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 import ReactMarkdown from 'react-markdown'
 import ArticleCard from '@/components/elements/ArticleCard'
 
 export default function PortfolioFilter3Col({articles, filters, filterField="content_types"}) {
-    // Isotope
-    const isotope = useRef()
-    const [filterKey, setFilterKey] = useState("*")
+
+    const [selectedFilters, setSelectedFilters] = useState([])
     const [filteredArticles, setFilteredArticles] = useState(articles)
 
-    const handleFilterKeyChange = useCallback((key) => () => {
-        let filtered = articles;
-        setFilteredArticles([])
+    useEffect(() => {
+        filterArticles()
+    }, [selectedFilters])
 
-        if (key !== "*") {
-            filtered = articles.filter(article => {
-                const filterOptions = article[filterField]?.data || []
-                const tagsClasses = filterOptions.map(t => t.attributes.slug)
-                return tagsClasses.includes(key)
-            })
+    const handleSelectFilter = (filterId) => () => {
+        let newSelectedFilters = [...selectedFilters]
+
+        if (filterId === '*') {
+            newSelectedFilters = []
+        } else if (selectedFilters.includes(filterId)) {
+            const filterRemoved = selectedFilters.filter(f => f !== filterId)
+            newSelectedFilters = filterRemoved
+        } else {
+            newSelectedFilters.push(filterId)
         }
 
-        setFilterKey(key)
-        setFilteredArticles(filtered)
-    }, [])
+        setSelectedFilters(newSelectedFilters)
+    }
 
-    const activeBtn = (value) => (value === filterKey ? "current" : "")
+    const filterArticles = () => {
+        setFilteredArticles([])
+        let filteredArticles = [...articles]
 
+        if (selectedFilters.length > 0) {
+            filteredArticles = filteredArticles.filter(article => {
+                const articleFilterFieldIds = article[filterField].data.map(f => f.attributes.slug)
+                const matches = articleFilterFieldIds.map(id => selectedFilters.includes(id))
+                
+                // use .every() to only allow events that match ALL the selected filters.
+                // use .some() to keep the events that match ANY of the selected filters.
+                return matches.some(m => m)
+              })
+        }
+
+        setFilteredArticles(filteredArticles)
+    }
+
+    const activeBtn = (value) => {
+        const noFilters = selectedFilters.length === 0
+
+        if (value === '*' && noFilters) {
+            return "current"
+        } else {
+            return selectedFilters.includes(value) ? "current" : ""
+        }
+
+    }
+
+    const articleCounts = useMemo(() => {
+        const counts = {}
+        filters.forEach(f => {
+            const articlesWithFilterField = articles.filter(article => {
+                const articleFilterFieldIds = article[filterField].data.map(f => f.attributes.slug)
+                return articleFilterFieldIds.includes(f.slug)
+            })
+
+            counts[f.slug] = articlesWithFilterField.length
+        })
+
+        return counts
+    })
 
     return (
         <>
@@ -38,11 +79,11 @@ export default function PortfolioFilter3Col({articles, filters, filterField="con
                         <div className="fliter_group">
                             <p className="title-small text-dark mb-0 mr_right_15">Filters:</p>
                             <ul className="project_filter dark clearfix">
-                                <li className={activeBtn("*")} onClick={handleFilterKeyChange("*")}>All</li>
+                                <li className={activeBtn('*')} onClick={handleSelectFilter("*")}>All</li>
                                 {
                                     filters.map(filter => {
                                         return(
-                                            <li key={filter.slug} className={activeBtn(filter.slug)} onClick={handleFilterKeyChange(filter.slug)}>{filter.name}</li>
+                                            <li key={filter.slug} className={activeBtn(filter.slug)} onClick={handleSelectFilter(filter.slug)}>{`${filter.name} (${articleCounts[filter.slug]})`}</li>
                                         )
                                     })
                                 }
@@ -59,17 +100,17 @@ export default function PortfolioFilter3Col({articles, filters, filterField="con
                         {
                             filteredArticles.map(article => {
                                 return (
-                                        <ArticleCard 
-                                            key={article.id}
-                                            article={article} 
-                                            tagsAttribute={filterField} 
-                                            showImage
-                                            imageTop
-                                            showTeaser
-                                            showDate
-                                            bgLight
-                                            showTags
-                                        />
+                                    <ArticleCard 
+                                        key={article.id}
+                                        article={article} 
+                                        tagsAttribute={filterField} 
+                                        showImage
+                                        imageTop
+                                        showTeaser
+                                        showDate
+                                        bgLight
+                                        showTags
+                                    />
                                 )
                             })
                         }
