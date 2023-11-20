5dc99c2d986eeba0dab58f31f29840c399cb49a7
Sharon <sharon.peishan.kennedy@gmail.com>
2023-11-20 15:53:32 -0500

    no indexing for now

--- pages/_document.js
+++ pages/_document.js
@@ -2,7 +2,9 @@ import { Head, Html, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
-            <Head />
+            <Head>
+                <meta name="robots" content="noindex, nofollow" />
+            </Head>
             <link rel="shortcut icon" href="/logomark.svg" />
             <link rel="preconnect" href="https://fonts.googleapis.com" />
             <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
